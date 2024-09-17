fa64bcaa232cb291ee6d7c7593afbf2a9f3b1f8d
Peter CONCHE <peter.conche@outlook.com>
2024-09-17 14:47:02 +0200

    test

--- SQL/liste-BDD-tri.sql
+++ SQL/liste-BDD-tri.sql
@@ -1,3 +1,6 @@
 SELECT recette.nom_recette, categorie.type_categorie, recette.tempsPreparationMns
 FROM recette INNER JOIN categorie ON recette.id_categorie = categorie.id_categorie
 ORDER BY tempsPreparationMns DESC;
+
+
+test sql
