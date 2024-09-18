359da03e2047aba7e47051be86276ffcd611f444
Peter CONCHE <peter.conche@outlook.com>
2024-09-18 13:43:48 +0200

    x

--- SQL/requete-tps-prepa-sup-30Mns.sql
+++ SQL/requete-tps-prepa-sup-30Mns.sql
@@ -1,1 +1,3 @@
 SELECT recette.id_recette, recette.nom_recette, recette.tempsPreparationMns FROM recette WHERE  recette.tempsPreparationMns >= 30;
+
+# affiche les recettes dont le temps de préparation est supérieur ou égal à 30 minutes
