e98b4328e5309612b73d3d5e27297aebb82bb088
Peter CONCHE <peter.conche@outlook.com>
2024-09-18 19:32:29 +0200

    x

---
+++ SQL/requete-liste-ingredients-recette.sql
@@ -0,0 +1,13 @@
+SELECT 
+    recette.nom_recette AS 'Recette',
+    ingredient.nom_ingredient AS 'Ingrédient',
+    cuisiner.`quantité` AS 'Quantité',
+    ingredient.uniteMesure AS 'Unité'
+FROM 
+    recette
+JOIN 
+    cuisiner ON recette.id_recette = cuisiner.id_recette
+JOIN 
+    ingredient ON cuisiner.id_ingredient = ingredient.id_ingredient
+WHERE 
+    recette.nom_recette = 'Pâtes à la "carbonara" à la française';
