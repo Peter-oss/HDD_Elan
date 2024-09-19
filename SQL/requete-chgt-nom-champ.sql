66c8206799686e4abc459f6aab6a5dd402121ec6
Peter CONCHE <peter.conche@outlook.com>
2024-09-19 10:43:27 +0200

    x

---
+++ SQL/requete-chgt-nom-champ.sql
@@ -0,0 +1,5 @@
+UPDATE recette SET recette.nom_recette = "Verrine à la confiture de lait de vache" WHERE id_recette =3;
+
+
+#Modifier le nom de la recette ayant comme identifiant id_recette = 3 (nom de la recette à votre 
+#convenance)
---
+++ SQL/requete-supr-recette.sql
@@ -0,0 +1,4 @@
+DELETE FROM cuisiner WHERE id_recette = 2;
+DELETE FROM recette WHERE id_recette = 2;
+
+#Supprimer la recette n°2 de la base de données y compris table associative !
