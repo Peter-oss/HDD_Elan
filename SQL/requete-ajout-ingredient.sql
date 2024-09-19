138508c55809d8d57fb3f76ceaa27e03b03dbf4b
Peter CONCHE <peter.conche@outlook.com>
2024-09-19 16:54:02 +0200

    x

---
+++ SQL/requete-ajout-ingredient.sql
@@ -0,0 +1,3 @@
+INSERT INTO ingredient (nom_ingredient, descrption, cout_ingredient, uniteMesure) VALUES ('Poivre moulu', 'Poivre moulu', 2.5, 'cueiller');
+
+#Ajouter un ingrédient en base de données : Poivre, unité : cuillère à café, prix : 2.5 €
