5028e22689d1e17dc0d8c846ce7524ce436b5c08
Peter CONCHE <peter.conche@outlook.com>
2024-09-19 08:35:15 +0200

    x

--- SQL/requete-liste-ingredients-recette.sql
+++ SQL/requete-liste-ingredients-recette.sql
@@ -11,3 +11,9 @@ JOIN
     ingredient ON cuisiner.id_ingredient = ingredient.id_ingredient
 WHERE 
     recette.nom_recette = 'Pâtes à la "carbonara" à la française';
+
+#Insérer une nouvelle recette : « Pâtes à la carbonara » dont la durée de réalisation est de 20 min avec 
+#les instructions de votre choix. Pensez à alimenter votre base de données en conséquence afin de 
+#pouvoir lister les détails de cette recettes (ingrédients)
+
+#La présentation n'est pas géniale mais ça marche
