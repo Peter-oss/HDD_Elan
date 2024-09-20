4c96a821227d22ee4a43ca2f50f6a7d3facf01f1
Peter CONCHE <peter.conche@outlook.com>
2024-09-20 14:32:32 +0200

    x

--- SQL/13-afficher_ingredient_poulet.sql
+++ SQL/13-afficher_ingredient_poulet.sql
@@ -1,3 +1,12 @@
+SELECT
+    recette.nom_recette,
+    cuisiner.id_ingredient
+    
+FROM
+    cuisiner
+        INNER JOIN recette
+            ON cuisiner.id_recette = recette.id_recette
+WHERE cuisiner.id_ingredient = 40;
 
-
+#40 = poulet par portions de 100 grammes
 #Afficher les recettes qui contiennent l’ingrédient « Poulet »
