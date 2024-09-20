a2136764b1fe15ccf921ddc32461959e80192668
Peter CONCHE <peter.conche@outlook.com>
2024-09-20 16:26:19 +0200

    x

--- SQL/14-Mettez à jour toutes les recettes.sql
+++ SQL/14-Mettez à jour toutes les recettes.sql
@@ -1,1 +1,8 @@
+UPDATE 
+    recette
+SET 
+    recette.tempsPreparationMns = recette.tempsPreparationMns - 5;
+
 #Mettez à jour toutes les recettes en diminuant leur temps de préparation de 5 minutes
+#Source : https://datascientest.com/update-sql-tout-savoir
+#Cette commande affichera une mise en garde car absence de 'Where' optionnel.
