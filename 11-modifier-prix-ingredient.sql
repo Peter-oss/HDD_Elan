dfa2eeba8fb4f4308bdbbc0faef17b5fef02ff39
Peter CONCHE <peter.conche@outlook.com>
2024-09-20 10:01:37 +0200

    x

---
+++ 11-modifier-prix-ingredient.sql
@@ -0,0 +1,4 @@
+UPDATE ingredient SET ingredient.cout_ingredient = 0.50 WHERE id_ingredient = 12;
+
+#Modifier le prix de l’ingrédient n°12 (prix à votre convenance)
+#Le beurre vient de passer de 0.02 le gramme à 0.50 le gramme en version de luxe
