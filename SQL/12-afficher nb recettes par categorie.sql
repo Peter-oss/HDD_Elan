7b40cf44e047a62217c3c970d51da8eb0f3f0c67
Peter CONCHE <peter.conche@outlook.com>
2024-09-20 11:51:25 +0200

    x

--- SQL/12-afficher nb recettes par categorie.sql
+++ SQL/12-afficher nb recettes par categorie.sql
@@ -1,5 +1,26 @@
-#Afficher le nombre de recettes par catégories : X entrées, Y plats, Z desserts
+#Afficher le nombre de recettes par catégories : 4 entrées, 5 plats, 6 desserts
 #Categories de disponibles :
 #4-Entree
 #5-Plat
 #6-Dessert
+
+/*
+SELECT recette.id_recette, recette.nom_recette, recette.tempsPreparationMns, COUNT(cuisiner.id_recette) AS nbIngredientsingredient
+FROM recette
+INNER JOIN cuisiner ON recette.id_recette = cuisiner.id_recette
+GROUP BY recette.id_recette
+ORDER BY tempsPreparationMns DESC;
+*/
+
+SELECT  categorie.id_categorie
+    ,   categorie.type_categorie
+    ,   COUNT(recette.id_recette) AS 'NbRecettes par catégorie'
+FROM    categorie   categorie
+    LEFT JOIN
+        recette
+        ON  categorie.id_categorie = recette.id_categorie
+GROUP BY categorie.id_categorie
+    ,   categorie.type_categorie;
+
+
+
