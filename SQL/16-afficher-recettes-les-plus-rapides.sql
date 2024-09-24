ffd5869ce92221cc92855b833a4fc805c94e0b80
Peter CONCHE <peter.conche@outlook.com>
2024-09-24 16:35:42 +0200

    x

--- SQL/16-afficher-recettes-les-plus-rapides.sql
+++ SQL/16-afficher-recettes-les-plus-rapides.sql
@@ -5,16 +5,8 @@ LIMIT 1;
 
 #Afficher la / les recette(s) les plus rapides à préparer (en minutes)
 #dans cet exemple il y a une seule recette à 5 minutes
+#Attention n'est pas valable si plusieurs recettes ont la meme durée
 
-#Afficher la / les recette(s) les plus rapides à préparer
-
-SELECT nom_recette, tempsPreparationMns
-FROM recette
-ORDER BY tempsPreparationMns ASC
-LIMIT 5;
-
-#Afficher la / les recette(s) les plus rapides à préparer (en minutes)
-#dans cet exemple il y a les 5 recettes prennant le moins de temps
 
 SELECT nom_recette, tempsPreparationMns
 FROM recette
