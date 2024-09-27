7292e10c3013aa068ea0914ea1e80d0b075c3750
Peter CONCHE <peter.conche@outlook.com>
2024-09-27 10:32:10 +0200

    x

--- SQL/19-Ajouter un nouvel ingrédient à une recette spécifique.sql
+++ SQL/19-Ajouter un nouvel ingrédient à une recette spécifique.sql
@@ -1,3 +1,10 @@
+-- Ajouter un nouvel ingrédient
+INSERT INTO ingredient (id_ingredient, nom_ingredient, descrption, cout_ingredient, uniteMesure) VALUES (id_ingredient, 'Tomate de luxe', 'Legume tomate', 1, 'La pièce');
 
+-- Associer l'ingrédient à une recette
+INSERT INTO cuisiner (id_recette, id_ingredient, quantité) VALUES (1, 44, 3);
+
+#Ajouter un nouvel ingrédient à une recette spécifique
+#id_recette = 1 = Tartare d'avocat et saumon, juste pour l'exemple
 
 #Ajouter un nouvel ingrédient à une recette spécifique
