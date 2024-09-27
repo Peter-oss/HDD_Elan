28e344b8e1dbc5db1d7d026101e0d8ee37b1dd6a
Peter CONCHE <peter.conche@outlook.com>
2024-09-27 08:58:06 +0200

    x

--- SQL/16-afficher-recettes-les-plus-rapides.sql
+++ SQL/16-afficher-recettes-les-plus-rapides.sql
@@ -1,15 +1,5 @@
 SELECT nom_recette, tempsPreparationMns
 FROM recette
-ORDER BY tempsPreparationMns ASC
-LIMIT 1;
-
-#Afficher la / les recette(s) les plus rapides à préparer (en minutes)
-#dans cet exemple il y a une seule recette à 5 minutes
-#Attention n'est pas valable si plusieurs recettes ont la meme durée
-
-
-SELECT nom_recette, tempsPreparationMns
-FROM recette
 WHERE tempsPreparationMns = (
     SELECT MIN(tempsPreparationMns)
     FROM recette
@@ -17,3 +7,4 @@ WHERE tempsPreparationMns = (
 
 #Affiche les recettes ayant le temps de preparation le plus court.
 #source : https://www.bing.com/chat?q=MySQL%20Afficher%20l%20%2F%20les%20recettes%20les%20plus%20rapides%20%C3%A0%20pr%C3%A9parer&qs=ds&form=SCCODX
+#Afficher la / les recette(s) les plus rapides à préparer (en minutes), ex-aequo si plusieurs recettes.
