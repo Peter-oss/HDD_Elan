20ee3534c47fccc4f2dbf5d88dfe197a7a84fc38
Peter CONCHE <peter.conche@outlook.com>
2024-09-27 11:35:48 +0200

    x

--- SQL/18-ingredients dans au moins 3 recettes.sql
+++ SQL/18-ingredients dans au moins 3 recettes.sql
@@ -1,7 +1,9 @@
-SELECT ingredient.id_ingredient, COUNT(recette.id_recette) AS recette_count
-FROM recette, ingredient
+SELECT ingredient.id_ingredient, ingredient.nom_ingredient, COUNT(recette.id_recette) AS recette_count
+FROM ingredient
+INNER JOIN cuisiner  ON ingredient.id_ingredient = cuisiner.id_ingredient
+INNER JOIN recette ON cuisiner.id_recette = recette.id_recette
 GROUP BY ingredient.id_ingredient
 HAVING recette_count >= 3;
 
 #mysql Trouver les ingrédients qui sont utilisés dans au moins 3 recettes
-#Requete à prendre avec des pincettes
+#utiliser nb de inner joint inferieur d1 au nbs de tables utilisées.
