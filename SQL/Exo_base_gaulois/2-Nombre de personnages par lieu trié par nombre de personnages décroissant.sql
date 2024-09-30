0c6d64988f2d57289ac4d9d7bf5a4217929484a4
Peter CONCHE <peter.conche@outlook.com>
2024-09-30 16:17:47 +0200

    x

--- SQL/Exo_base_gaulois/2-Nombre de personnages par lieu trié par nombre de personnages décroissant.sql
+++ SQL/Exo_base_gaulois/2-Nombre de personnages par lieu trié par nombre de personnages décroissant.sql
@@ -1,2 +1,6 @@
 #Nombre de personnages par lieu (trié par nombre de personnages décroissant).
 
+SELECT id_lieu, COUNT(*) AS nombre_de_personnages
+FROM personnage
+GROUP BY id_lieu
+ORDER BY nombre_de_personnages DESC;
