c854c8cd1bddaa58362a6b75823bab3988a6f7d9
Peter CONCHE <peter.conche@outlook.com>
2024-10-01 09:21:51 +0200

    x

--- SQL/Exo_base_gaulois/3-Nom des personnages + spécialité + adresse et lieu d'habitation, triés par lieu puis par nom  de personnage.sql
+++ SQL/Exo_base_gaulois/3-Nom des personnages + spécialité + adresse et lieu d'habitation, triés par lieu puis par nom  de personnage.sql
@@ -1,2 +1,6 @@
 #Nom des personnages + spécialité + adresse et lieu d'habitation, triés par lieu puis par nom de personnage
 
+SELECT NOM_personnage, NOM_SPECIALITE, NOM_LIEU
+FROM personnage v, specialite s, lieu l
+WHERE v.ID_SPECIALITE = s.ID_SPECIALITE
+AND l.ID_LIEU = v.ID_LIEU;
