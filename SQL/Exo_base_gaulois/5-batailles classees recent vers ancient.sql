d59c98eaa22e6c0d3e38ec2791efa5a27270f308
Peter CONCHE <peter.conche@outlook.com>
2024-10-01 14:09:13 +0200

    x

--- SQL/Exo_base_gaulois/5-batailles classees recent vers ancient.sql
+++ SQL/Exo_base_gaulois/5-batailles classees recent vers ancient.sql
@@ -1,2 +1,7 @@
-#Exo-5 Nom, date et lieu des batailles, classées de la plus récente à la plus ancienne (dates affichées au format jj/mm/aaaa).
+#Exo-5 Nom, date et lieu des batailles, classées de la plus récente à la plus ancienne (dates affichées au format jj/mm/aaaa)
 
+SELECT nom_bataille, nom_lieu, date_format(date_bataille, "%d/%m/%Y")
+FROM lieu
+JOIN bataille
+ON lieu.id_lieu = bataille.id_lieu
+ORDER BY date_bataille DESC;
