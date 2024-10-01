3c05a25746da31844269a72cc4a30137bb98fddf
Peter CONCHE <peter.conche@outlook.com>
2024-10-01 11:43:59 +0200

    x

--- SQL/Exo_base_gaulois/4-Nom des spécialités avec nombre de personnages par spécialité-trié par nombre de  personnages décroissant.sql
+++ SQL/Exo_base_gaulois/4-Nom des spécialités avec nombre de personnages par spécialité-trié par nombre de  personnages décroissant.sql
@@ -1,1 +1,8 @@
 #Nom des spécialités avec nombre de personnages par spécialité (trié par nombre de personnages décroissant).
+
+SELECT nom_specialite, count(nom_personnage)
+FROM specialite
+JOIN personnage
+ON specialite.id_specialite = personnage.id_specialite
+GROUP BY nom_specialite
+ORDER BY COUNT(nom_personnage) DESC
