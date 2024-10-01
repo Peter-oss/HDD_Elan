76beeadac467e2164cce1a1328b83d146c6a1e87
Peter CONCHE <peter.conche@outlook.com>
2024-10-01 14:43:03 +0200

    x

---
+++ SQL/Exo_base_gaulois/6-Nom potion cout decroissant.sql
@@ -0,0 +1,9 @@
+#Exo-6 Nom des potions + coût de réalisation de la potion (trié par coût décroissant).
+
+SELECT nom_potion, cout_ingredient
+FROM potion
+JOIN ingredient
+ON potion.id_potion = ingredient.id_ingredient
+JOIN composer
+ON ingredient.id_ingredient = composer.id_potion
+ORDER BY cout_ingredient DESC
