051bbb1220848a753116426f227243be2b1df050
Peter CONCHE <peter.conche@outlook.com>
2024-10-02 14:29:43 +0200

    x

--- SQL/Exo_base_gaulois/8-Nom du ou des personnages qui ont pris le plus de casques dans la bataille 'Bataille du village gaulois'.sql
+++ SQL/Exo_base_gaulois/8-Nom du ou des personnages qui ont pris le plus de casques dans la bataille 'Bataille du village gaulois'.sql
@@ -1,2 +1,12 @@
-8-Nom du ou des personnages qui ont pris le plus de casques dans la bataille 'Bataille du village gaulois'.
+#Exo-8 Nom du ou des personnages qui ont pris le plus de casques dans la bataille 'Bataille du village gaulois'.
+#Les lettres indiquées après les noms de 'tables' sont des alias.
 
+SELECT nom_personnage, SUM(pc.qte) AS nbCasques
+FROM personnage v, prendre_casque pc, bataille b 
+WHERE v.id_personnage = pc.id_personnage
+AND pc.ID_BATAILLE = b.ID_BATAILLE 
+AND b.NOM_BATAILLE = "Bataille du village gaulois"
+GROUP BY v.nom_personnage 
+ORDER BY nbCasques DESC
+
+#Premier jet.
