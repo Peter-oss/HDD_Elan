bc50c29914d245d711c0d25f1bc7f2ffbe669ee7
Peter CONCHE <peter.conche@outlook.com>
2024-10-02 17:03:48 +0200

    x

--- SQL/Exo_base_gaulois/8-Nom du ou des personnages qui ont pris le plus de casques dans la bataille 'Bataille du village gaulois'.sql
+++ SQL/Exo_base_gaulois/8-Nom du ou des personnages qui ont pris le plus de casques dans la bataille 'Bataille du village gaulois'.sql
@@ -1,4 +1,7 @@
 #Exo-8 Nom du ou des personnages qui ont pris le plus de casques dans la bataille 'Bataille du village gaulois'.
+
+#Version 1
+
 #Les lettres indiquées après les noms de 'tables' sont des alias.
 
 SELECT nom_personnage, SUM(pc.qte) AS nbCasques
@@ -13,3 +16,22 @@ LIMIT 1;
 #Premier jet.
 #Requete imbriquée
 #Celui qui a récupéré le plus de casques : LIMIT 1; celui qui en a le plus.
+
+#Version 2
+
+SELECT p.nom_personnage, SUM(pc.qte) AS nb_casques
+FROM personnage p, bataille b, prendre_casque pc
+WHERE p.id_personnage = pc.id_personnage
+AND pc.id_bataille = b.id_bataille
+AND b.nom_bataille = 'Bataille du village gaulois'
+GROUP BY p.id_personnage
+HAVING nb_casques >= ALL(
+SELECT SUM(pc.qte)
+    FROM prendre_casque pc, bataille b
+    WHERE b.id_bataille = pc.id_bataille
+    AND b.nom_bataille = 'Bataille du village gaulois'
+    GROUP BY pc.id_personnage
+)
+
+#Ici Having au lieu de Where, requete imbriquée avec sous-requete, à utiliser si plusieurs personnages ont pris le mm nb de casques.
+#Cette version est à préférer si des personnages ont les memes scores.
