1097cb36998538f5816a1a0ae9fd0552050f1a20
Peter CONCHE <peter.conche@outlook.com>
2024-10-02 11:50:32 +0200

    x

--- SQL/Exo_base_gaulois/7-Nom des ingrédients + coût + quantité de chaque ingrédient qui composent la potion Santé.sql
+++ SQL/Exo_base_gaulois/7-Nom des ingrédients + coût + quantité de chaque ingrédient qui composent la potion Santé.sql
@@ -1,15 +1,13 @@
 #Exo-7 Nom des ingrédients + coût + quantité de chaque ingrédient qui composent la potion 'Santé'. Potion=3
 
 SELECT 
-nom_potion, 
-cout_ingredient AS cout_ingredient,
-nom_ingredient AS nb_ingredient,
-qte,
-cout_ingredient * qte AS cout_total
+cout_ingredient AS cout_unitaire_de_chaque_ingredient,
+nom_ingredient AS nom_ingredient,
+qte AS Quantités,
+cout_ingredient * qte AS cout_total_de_chaque_ingredient
 FROM potion
 INNER JOIN composer
 ON composer.id_potion = potion.id_potion
 INNER JOIN ingredient
 ON composer.id_ingredient = ingredient.id_ingredient
 WHERE potion.id_potion = 3;
-
